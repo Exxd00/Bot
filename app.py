20f8e73ad671e2783dd7060d93217ec4dc679cc2
Emad <155592538+Exxd00@users.noreply.github.com>
2025-06-06 03:06:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -66,3 +66,8 @@ def run_action():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+# ✅ لتشغيل الخادم على Render
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
